24a31624c436537693a010eee91f53ab739889cf
DESKTOP-PEFJP14\zaks <glcasian@outlook.com>
2022-07-14 17:06:21 +0800

    添加记录

---
+++ 19.控件ComboBox/Models.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.控件ComboBox {
+    internal class Models : ObservableCollection<string> {
+
+        public Models() {
+
+            Add("Spain");
+            Add("France");
+            Add("Peru");
+            Add("Mexico");
+            Add("Italy");
+        }
+
+    }
+}
