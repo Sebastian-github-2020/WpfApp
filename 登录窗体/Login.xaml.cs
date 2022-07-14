95bf92a8b47336a01ed06e7c39288263815b0f09
Sebastian-github-2020 <zaksvisitor@gmail.com>
2022-07-14 21:13:46 +0800

    小盒子增加一部分功能，添加登录跳转

---
+++ 登录窗体/Login.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace 登录窗体 {
+    /// <summary>
+    /// Login.xaml 的交互逻辑
+    /// </summary>
+    public partial class Login : Window {
+        public Login() {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e) {
+            // 1. 验证账户密码
+            if(this.username.Text.Equals("admin") && this.password.Password.Equals("admin")) {
+                MessageBox.Show("登录成功");
+                // 转到新窗口
+                MainWindow mainWindow = new MainWindow();
+                //显示新窗口
+                mainWindow.Show();
+                //关闭当前窗口
+                this.Close();
+            } else {
+                MessageBox.Show("登录失败");
+            }
+        }
+    }
+}
