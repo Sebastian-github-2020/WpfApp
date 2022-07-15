32e5bef0879d4ede1b6f379cf646b3cca392eb4f
DESKTOP-PEFJP14\zaks <glcasian@outlook.com>
2022-07-15 16:44:25 +0800

    动画

---
+++ 24.动画基础/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace _24.动画基础 {
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 按钮点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_Click(object sender, RoutedEventArgs e) {
+            // 1. 创建动画对象
+            DoubleAnimation animation = new DoubleAnimation();
+            // 2. 指定起始位置 From和to都可以省略
+            //animation.From = Btn.ActualWidth;//ActualWidth 是对象呈现的宽度  width是设定的宽度
+            //animation.To = Btn.ActualWidth - 130;
+
+            animation.By = -200; // 不指定起始值 使用by ，下面执行动画 指定了 依赖属性 
+
+            //3. 指定动画事件
+            animation.Duration = TimeSpan.FromSeconds(1);
+            // 4. 动画 反向播放 运行时间也加倍
+            animation.AutoReverse = true;
+            // 设置缓动值 开始慢 然后增速
+            animation.AccelerationRatio = 1;
+            // 设置重复次数 根据参数来决定的 可以传入时间 设置重复的时间 也可以一直重复
+            //animation.RepeatBehavior = new RepeatBehavior(2);
+            //animation.RepeatBehavior = new RepeatBehavior(TimeSpan.FromSeconds(5));
+            animation.RepeatBehavior = RepeatBehavior.Forever;
+
+            // 5. 执行动画
+            Btn.BeginAnimation(Button.WidthProperty, animation);
+
+
+
+        }
+    }
+}
---
+++ 29.代码实现故事板/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace _29.代码实现故事板 {
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e) {
+            // 创建两个 动画
+            DoubleAnimation btnAnimation = new DoubleAnimation(500, TimeSpan.FromSeconds(2));
+            btnAnimation.From = Btn.ActualWidth;
+
+
+            DoubleAnimation btnAnimation1 = new DoubleAnimation(300, TimeSpan.FromSeconds(2));
+            btnAnimation1.From = Btn.ActualHeight;
+
+
+            // 创建故事板
+            Storyboard storyboard = new Storyboard();
+
+            // 使用Storyboard 类 指定运行动画的xaml节点的name 
+            Storyboard.SetTargetName(storyboard, "Btn");
+            // 设置依赖的属性
+            Storyboard.SetTargetProperty(btnAnimation, new PropertyPath(Button.WidthProperty));
+
+            Storyboard.SetTargetName(storyboard, "Btn");
+            Storyboard.SetTargetProperty(btnAnimation1, new PropertyPath(Button.HeightProperty));
+
+            // 设置 故事板属性
+            storyboard.Duration = TimeSpan.FromSeconds(2);
+            storyboard.AutoReverse = true;
+            storyboard.RepeatBehavior = RepeatBehavior.Forever;
+
+            // 添加动画到故事板
+            storyboard.Children.Add(btnAnimation);
+            storyboard.Children.Add(btnAnimation1);
+
+            // 执行故事板
+            storyboard.Begin(this);
+        }
+    }
+}
