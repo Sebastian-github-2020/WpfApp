1358ae1b606dbd1c6f0cee2bc8eed84956e44ced
Sebastian-github-2020 <zaksvisitor@gmail.com>
2023-02-28 22:46:53 +0800

    练习

---
+++ 31.MvvmLightDemo/Models/StudentModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31.MvvmLightDemo.Models
+{
+    public class StudentModel
+    {
+        private int id;
+
+        public int Id {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+
+        private string name;
+
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+    }
+}
