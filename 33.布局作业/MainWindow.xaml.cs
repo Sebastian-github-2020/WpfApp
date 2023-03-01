5678e45fb0021e0470b3220cbb7fd494d63ede46
Sebastian-github-2020 <zaksvisitor@gmail.com>
2023-03-01 22:17:00 +0800

    布局作业，增加头像，增加最大最小关闭按钮

--- 33.布局作业/MainWindow.xaml.cs
+++ 33.布局作业/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,17 +14,52 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace _33.布局作业 {
+namespace _33.布局作业
+{
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow : Window
+    {
         public MainWindow() {
             InitializeComponent();
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
             this.DragMove();
+        }
+        /// <summary>
+        /// 最小化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void minBtn_Click(object sender, RoutedEventArgs e) {
+            this.WindowState = WindowState.Minimized;
+        }
+        /// <summary>
+        /// 最大化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void maxBtn_Click(object sender, RoutedEventArgs e) {
+            Debug.WriteLine(this.WindowState);
+            this.WindowState = this.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+
+        }
+        /// <summary>
+        /// 关闭窗口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void closeBtn_Click(object sender, RoutedEventArgs e) {
+            var res = MessageBox.Show("是否确定要关闭窗口", "警告", MessageBoxButton.YesNo);
+            if(res == MessageBoxResult.Yes)
+            {
+                // 关闭所有线程
+                Environment.Exit(Environment.ExitCode);
+                //this.Close();
+            }
+
         }
     }
 }
