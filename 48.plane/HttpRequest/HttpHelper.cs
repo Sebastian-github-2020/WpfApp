a04fc40c5f21cd94e07c834945b1c569c9092ca8
DESKTOP-PEFJP14\zaks <glcasian@outlook.com>
2023-04-07 17:20:06 +0800

    http工具类 配置header uuid和sign

--- 48.plane/HttpRequest/HttpHelper.cs
+++ 48.plane/HttpRequest/HttpHelper.cs
@@ -5,24 +5,37 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using _48.plane.Tools;
 namespace _48.plane.HttpRequest {
     /// <summary>
     /// http工具类
     /// </summary>
     public class HttpHelper {
         private static readonly HttpClient _httpClient = new HttpClient();
+        private static string SecritKey = "9&N4orgck9M!rh2#Wpfyg2Q!teDds8Bl";
         public static HttpClient GetInstance() {
             _httpClient.BaseAddress = new Uri("https://5981aa.com");
-            _httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
+            HeaderConfig();
+
             return _httpClient;
         }
 
         public static async Task RequestGet(string url) {
+            HeaderConfig();
             HttpResponseMessage response = await _httpClient.GetAsync(url);
             if(response.IsSuccessStatusCode) {
                 Debug.Write(response.Content);
             }
+        }
+
+        /// <summary>
+        /// 配置header uuid 和sign
+        /// </summary>
+        public static void HeaderConfig() {
+            string uuid = Tool.MakeUUID();
+            _httpClient.DefaultRequestHeaders.Add("x-auth-uu", uuid);
+            _httpClient.DefaultRequestHeaders.Add("x-auth-sign", Tool.MakeSign(uuid, SecritKey));
+            _httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
         }
     }
 }
